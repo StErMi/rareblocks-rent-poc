c23af472dfa187e654cd2f861e81f8eba3fdf33f
Emanuele Ricci <stermi@gmail.com>
2022-01-13 09:06:48 +0100

    updated stake contract tests

--- test/stake.ts
+++ test/stake.ts
@@ -30,6 +30,7 @@ describe('Stake Contract', () => {
   let staker1: SignerWithAddress;
   let staker2: SignerWithAddress;
   let staker3: SignerWithAddress;
+  let staker4: SignerWithAddress;
   let renter1: SignerWithAddress;
   let renter2: SignerWithAddress;
   let addrs: SignerWithAddress[];
@@ -47,7 +48,7 @@ describe('Stake Contract', () => {
   };
 
   beforeEach(async () => {
-    [owner, tresury, staker1, staker2, staker3, renter1, renter2, ...addrs] = await ethers.getSigners();
+    [owner, tresury, staker1, staker2, staker3, staker4, renter1, renter2, ...addrs] = await ethers.getSigners();
 
     rareBlocks = (await deployContract(owner, RareBlocksArtifact)) as RareBlocks;
 
@@ -86,6 +87,9 @@ describe('Stake Contract', () => {
     await rareBlocks.connect(staker3).mint(staker3.address, 1, {value: ethers.utils.parseEther('0.08')});
     await rareBlocks.connect(staker3).mint(staker3.address, 1, {value: ethers.utils.parseEther('0.08')});
 
+    // Mint rareblock for the staker4
+    await rareBlocks.connect(staker4).mint(staker4.address, 1, {value: ethers.utils.parseEther('0.08')});
+
     // Approve the contract to interact with the NFT
     await rareBlocks.connect(staker1).approve(stake.address, 16);
     await rareBlocks.connect(staker2).approve(stake.address, 17);
@@ -93,6 +97,7 @@ describe('Stake Contract', () => {
     await rareBlocks.connect(staker3).approve(stake.address, 19);
     await rareBlocks.connect(staker3).approve(stake.address, 20);
     await rareBlocks.connect(staker3).approve(stake.address, 21);
+    await rareBlocks.connect(staker4).approve(stake.address, 22);
   });
 
   describe('Test stake()', () => {
@@ -108,7 +113,7 @@ describe('Stake Contract', () => {
     });
     it('stake a token that the owner does not have approved yet to be transferred to the stake contract', async () => {
       await rareBlocks.connect(staker1).mint(staker1.address, 1, {value: ethers.utils.parseEther('0.08')});
-      const tx = stake.connect(staker1).stake(22);
+      const tx = stake.connect(staker1).stake(23);
 
       await expect(tx).to.be.revertedWith('ERC721: transfer caller is not owner nor approved');
     });
@@ -118,6 +123,14 @@ describe('Stake Contract', () => {
       const tx = stake.connect(staker1).stake(tokenID);
 
       await expect(tx).to.be.revertedWith('TOKEN_NOT_OWNED');
+    });
+    it('stake a token when the contract is paused', async () => {
+      await stake.connect(owner).pauseStake();
+
+      const tokenID = 16;
+      const tx = stake.connect(staker1).stake(tokenID);
+
+      await expect(tx).to.be.revertedWith('Pausable: paused');
     });
     it('stake it when payoutID is 0', async () => {
       const tokenID = 16;
@@ -144,6 +157,51 @@ describe('Stake Contract', () => {
 
       // Check that the owner of the token is the staker
       expect(await rareBlocks.ownerOf(tokenID)).to.eq(stake.address);
+    });
+  });
+
+  describe('Test unstake()', () => {
+    beforeEach(async () => {
+      await stake.connect(staker4).stake(22);
+    });
+
+    it("unstake a token you don't own", async () => {
+      const tx = stake.connect(staker4).unstake(1);
+
+      await expect(tx).to.be.revertedWith('NOT_TOKEN_OWNER');
+    });
+
+    it('unstake a token when the contract is paused', async () => {
+      await stake.connect(owner).pauseStake();
+
+      const tokenID = 22;
+      const tx = stake.connect(staker4).unstake(tokenID);
+
+      await expect(tx).to.be.revertedWith('Pausable: paused');
+    });
+    it('unstake it when payoutID is 0', async () => {
+      const tokenID = 22;
+      const totalStakedTokenBefore = await stake.totalStakedToken();
+      const totalStakedTokenNextCycle = await stake.totalStakedTokenNextCycle();
+
+      const tx = stake.connect(staker4).unstake(tokenID);
+
+      // Event is correctly emitted
+      await expect(tx).to.emit(stake, 'Unstaked').withArgs(staker4.address, tokenID);
+
+      // Number of totalStakedTokenBefore is updated
+      expect(await stake.totalStakedToken()).to.eq(totalStakedTokenBefore.sub(1));
+
+      // Number of totalStakedTokenNextCycle is the same
+      expect(await stake.totalStakedTokenNextCycle()).to.eq(totalStakedTokenNextCycle);
+
+      // Stake of the user has been updated
+      const userStakeBefore = await stake.stakes(tokenID);
+      expect(userStakeBefore.stakeTime.toNumber()).to.eq(0);
+      expect(userStakeBefore.owner).to.eq(ethers.constants.AddressZero);
+
+      // Check that the owner of the token is the staker
+      expect(await rareBlocks.ownerOf(tokenID)).to.eq(staker4.address);
     });
   });
 });
