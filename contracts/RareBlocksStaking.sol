0265f2f2ab8f07ba105c7fc5a03c66065baaa0ed
Emanuele Ricci <stermi@gmail.com>
2022-01-29 15:16:39 +0100

    removed hardhat console import

--- contracts/RareBlocksStaking.sol
+++ contracts/RareBlocksStaking.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.7;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
